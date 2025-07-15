0c8a50e99f1a873372521f4462a94e0ca5f5e7c9
Alejandro0726272829 <alexmanzanohdz26@gmail.com>
2025-07-15 12:45:34 -0600

    Update UsuariosController.cs

--- ConecteeApi/Controllers/UsuariosController.cs
+++ ConecteeApi/Controllers/UsuariosController.cs
@@ -2,10 +2,6 @@ using ConecteeApi.Helpers;
 using ConecteeApi.Interfaces;
 using ConecteeApi.Models;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.IdentityModel.Tokens;
-using System.IdentityModel.Tokens.Jwt;
-using System.Security.Claims;
-using System.Text;
 
 namespace ConecteeApi.Controllers
 {
@@ -14,23 +10,10 @@ namespace ConecteeApi.Controllers
     public class UsuariosController : ControllerBase
     {
         private readonly IBaseService<Usuario> _usuarioService;
-        private readonly IConfiguration _configuration;
 
-        public UsuariosController(IBaseService<Usuario> usuarioService, IConfiguration configuration)
+        public UsuariosController(IBaseService<Usuario> usuarioService)
         {
             _usuarioService = usuarioService;
-            _configuration = configuration;
-        }
-
-        [HttpPost("login")]
-        public async Task<IActionResult> Login([FromBody] LoginRequest login)
-        {
-            var user = await AuthenticateUserAsync(login.Correo, login.Password);
-            if (user == null)
-                return Unauthorized("Usuario o contraseña incorrectos");
-
-            var tokenString = GenerateJWT(user);
-            return Ok(new { Token = tokenString });
         }
 
         [HttpGet]
@@ -74,43 +57,6 @@ namespace ConecteeApi.Controllers
 
             await _usuarioService.DeleteAsync(id);
             return NoContent();
-        }
-
-        private async Task<Usuario?> AuthenticateUserAsync(string correo, string password)
-        {
-            var users = await _usuarioService.GetAllAsync();
-
-            foreach (var user in users)
-            {
-                if (user.Correo == correo && PasswordHasher.VerifyPassword(password, user.Contrasena))
-                    return user;
-            }
-            return null;
-        }
-
-        private string GenerateJWT(Usuario user)
-        {
-            var key = _configuration["Jwt:Key"];
-            if (string.IsNullOrEmpty(key))
-                throw new Exception("JWT Key no configurada en appsettings.json");
-
-            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(key));
-            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
-
-            var claims = new[]
-            {
-                new Claim(ClaimTypes.Name, user.Correo!),
-                new Claim(ClaimTypes.NameIdentifier, user.Id!)
-            };
-
-            var token = new JwtSecurityToken(
-                issuer: _configuration["Jwt:Issuer"],
-                audience: _configuration["Jwt:Audience"],
-                claims: claims,
-                expires: DateTime.Now.AddHours(2),
-                signingCredentials: credentials);
-
-            return new JwtSecurityTokenHandler().WriteToken(token);
         }
     }
 }
