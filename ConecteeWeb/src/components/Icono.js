4ef4a961e6686b7477a466e8a759db5b09b24f47
Alejandro0726272829 <alexmanzanohdz26@gmail.com>
2025-07-19 04:38:59 -0600

    Update Icono.js

--- ConecteeWeb/src/components/Icono.js
+++ ConecteeWeb/src/components/Icono.js
@@ -1,32 +1,22 @@
 import React from 'react';
-import barnIcon from '../icons/barn_5988720.png';
-import truckIcon from '../icons/truck_713311.png';
+import camion from '../icons/camion.png';
+import casa from '../icons/casa.png';
 
-function Iconos() {
+function Icono() {
   return (
-    <div style={{ display: 'flex', gap: '1rem', alignItems: 'center' }}>
-      <div>
-        <img src={barnIcon} alt="Icono de bodega" width={64} height={64} />
-        <p>Bodega</p>
+    <div style={{ display: 'flex', justifyContent: 'center', gap: '40px', marginTop: '20px' }}>
+      <div style={{ textAlign: 'center' }}>
+        <img src={camion} alt="Camión" width="64" height="64" />
+        <p style={{ color: 'white' }}>Camión</p>
       </div>
-      <div>
-        <img src={truckIcon} alt="Icono de camión" width={64} height={64} />
-        <p>Camión</p>
+      <div style={{ textAlign: 'center' }}>
+        <img src={casa} alt="Casa" width="64" height="64" />
+        <p style={{ color: 'white' }}>Casa</p>
       </div>
     </div>
   );
 }
 
-function Footer() {
-  return (
-    <footer style={{ fontSize: '12px', textAlign: 'center', marginTop: '1rem' }}>
-      <p>
-        Icono de bodega por <a href="http://awicons.com" target="_blank" rel="noopener noreferrer">Lokas Software / AWIcons</a> &nbsp;|&nbsp; 
-        Icono de camión por <a href="https://www.vecteezy.com" target="_blank" rel="noopener noreferrer">Vecteezy</a>
-      </p>
-    </footer>
-  );
-}
+export default Icono;
 
-export { Iconos, Footer };
 
