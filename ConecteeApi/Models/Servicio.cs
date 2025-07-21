153636f36b27afe7d379122b1c75486d6603a720
Alejandro0726272829 <alexmanzanohdz26@gmail.com>
2025-07-20 23:47:27 -0600

    Update Servicio.cs

--- ConecteeApi/Models/Servicio.cs
+++ ConecteeApi/Models/Servicio.cs
@@ -4,36 +4,51 @@ using System;
 
 namespace ConecteeApi.Models
 {
-   public class Servicio
-{
-    [BsonId]
-    [BsonRepresentation(BsonType.ObjectId)]
-    public string? Id { get; set; }
+    public class Servicio
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string? Id { get; set; }
 
-    [BsonElement("usuarioId")]
-    public string UsuarioId { get; set; } = null!;
+        [BsonElement("usuarioId")]
+        public string UsuarioId { get; set; } = null!;
 
-    [BsonElement("descripcion")]
-    public string Descripcion { get; set; } = null!;
+        [BsonElement("descripcion")]
+        public string Descripcion { get; set; } = null!;
 
-    [BsonElement("origen")]
-    public string Origen { get; set; } = null!;
+        [BsonElement("origen")]
+        public string Origen { get; set; } = null!;
 
-    [BsonElement("destino")]
-    public string Destino { get; set; } = null!;
+        [BsonElement("destino")]
+        public string Destino { get; set; } = null!;
 
-    [BsonElement("costo")] // 🔥 ¡Este debe existir!
-    public decimal Costo { get; set; }
+        [BsonElement("costo")]
+        public decimal Costo { get; set; }
 
-    [BsonElement("estado")]
-    public string Estado { get; set; } = null!;
+        [BsonElement("estado")]
+        public string Estado { get; set; } = null!;
 
-    [BsonElement("fecha")]
-    public DateTime Fecha { get; set; }
+        [BsonElement("fecha")]
+        public DateTime Fecha { get; set; }
 
-    [BsonElement("fechaSolicitud")]
-    public DateTime FechaSolicitud { get; set; }
+        [BsonElement("fechaSolicitud")]
+        public DateTime FechaSolicitud { get; set; }
+
+        [BsonElement("coordenadaOrigen")]
+        public Punto CoordenadaOrigen { get; set; } = null!;
+
+        [BsonElement("coordenadaDestino")]
+        public Punto CoordenadaDestino { get; set; } = null!;
+    }
+
+    public class Punto
+    {
+        [BsonElement("lat")]
+        public double Lat { get; set; }
+
+        [BsonElement("lng")]
+        public double Lng { get; set; }
+    }
 }
 
-}
 
