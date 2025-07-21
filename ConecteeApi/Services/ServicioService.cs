e57187653a30aec60f642137cc9c2f188e81ed44
Alejandro0726272829 <alexmanzanohdz26@gmail.com>
2025-07-20 23:55:44 -0600

    Update ServicioService.cs

--- ConecteeApi/Services/ServicioService.cs
+++ ConecteeApi/Services/ServicioService.cs
@@ -16,10 +16,20 @@ namespace ConecteeApi.Services
             _servicios = database.GetCollection<Servicio>(settings.Value.ServicioCollectionName);
         }
 
-        public async Task<List<Servicio>> GetAllAsync() => await _servicios.Find(_ => true).ToListAsync();
-        public async Task<Servicio?> GetByIdAsync(string id) => await _servicios.Find(s => s.Id == id).FirstOrDefaultAsync();
-        public async Task CreateAsync(Servicio servicio) => await _servicios.InsertOneAsync(servicio);
-        public async Task UpdateAsync(string id, Servicio servicio) => await _servicios.ReplaceOneAsync(s => s.Id == id, servicio);
-        public async Task DeleteAsync(string id) => await _servicios.DeleteOneAsync(s => s.Id == id);
+        public async Task<List<Servicio>> GetAllAsync() =>
+            await _servicios.Find(_ => true).ToListAsync();
+
+        public async Task<Servicio?> GetByIdAsync(string id) =>
+            await _servicios.Find(s => s.Id == id).FirstOrDefaultAsync();
+
+        public async Task CreateAsync(Servicio servicio) =>
+            await _servicios.InsertOneAsync(servicio);
+
+        public async Task UpdateAsync(string id, Servicio servicio) =>
+            await _servicios.ReplaceOneAsync(s => s.Id == id, servicio);
+
+        public async Task DeleteAsync(string id) =>
+            await _servicios.DeleteOneAsync(s => s.Id == id);
     }
 }
+
